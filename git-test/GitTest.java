c8d1b99f8fae6d0b2170d3931a4c3264cb2cdaa7
fiona <fiona@163.com>
2024-05-30 21:45:56 +0800

    hot-fix second commit

--- git-test/GitTest.java
+++ git-test/GitTest.java
@@ -13,5 +13,10 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
