e44345aabc5040295a76789c014a69048cf14909
fiona <fiona@163.com>
2024-05-30 21:28:33 +0800

    first commit

---
+++ git-test/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: GitTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 蠢
+ * @Create 2024/5/30 21:21
+ * @Version 17.0.2
+ */
+public class GitTest {
+    public  static  void main(String[] args){
+        System.out.println("hello git");
+    }
+}
